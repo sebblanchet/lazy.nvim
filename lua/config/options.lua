7fb2d9b699ca9cf74dd76e07e7fc23347bd683f1
seb blanchet <s3blanch@uwaterloo.ca>
2024-08-24 12:28:51 -0400

    port ops + start mappings

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,77 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local g = vim.g -- Global variables
+local opt = vim.opt -- with vim.opt we can set global, window and buffer settings, acting like :set in vimscript
+local set = vim.opt
+
+-- spell check + disable mouse + global paste
+vim.cmd([[
+set spelllang=en_us
+set clipboard+=unnamedplus
+set mouse=
+]])
+
+-- turn off macro reording
+vim.cmd([[
+map q <Nop>
+]])
+
+-- ignore annoying swaps
+opt.swapfile = false -- Don't use swapfile
+opt.backup = false
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+opt.undofile = true
+
+-- numbers
+opt.number = true -- bool: Show line numbers
+opt.relativenumber = true -- bool: Show relative line numbers
+opt.scrolloff = 4 -- int:  Min num lines of context
+opt.signcolumn = "yes" -- str:  Show the sign column
+opt.numberwidth = 4 -- gutter width
+opt.cursorline = true -- display cursor line
+-- opt.cursoropts= "line" -- display whole row
+opt.laststatus = 3 -- show just one status line
+
+-- [[ Filetypes ]]
+opt.encoding = "utf8" -- str:  String encoding to use
+opt.fileencoding = "utf8" -- str:  File encoding to use
+
+-- [[ Theme ]]
+opt.syntax = "ON" -- str:  Allow syntax highlighting
+opt.termguicolors = true -- bool: If term supports ui color then enable
+
+-- [[ Search ]]
+opt.ignorecase = true -- bool: Ignore case in search patterns
+opt.smartcase = true -- bool: Override ignorecase if search contains capitals
+opt.incsearch = true -- bool: Use incremental search
+opt.hlsearch = false -- bool: Highlight search matches
+
+-- [[ Whitespace ]]
+opt.smartindent = true -- Autoindent new lines
+opt.expandtab = true -- bool: Use spaces instead of tabs
+opt.shiftwidth = 2 -- num:  Size of an indent
+opt.softtabstop = 2 -- num:  Number of spaces tabs count for in insert mode
+opt.tabstop = 2 -- num:  Number of spaces tabs count for
+
+-- [[ Splits ]]
+opt.splitright = true -- bool: Place new window to right of current one
+opt.splitbelow = true -- bool: Place new window below the current one
+
+-- Fixed column for diagnostics to appear
+-- Show autodiagnostic popup on cursor hover_range
+-- Goto previous / next diagnostic warning / error
+-- Show inlay_hints more frequently
+vim.o.updatetime = 350
+set.signcolumn = "yes"
+vim.cmd([[
+autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
+]])
+
+-- color
+g.t_co = 256
+
+-- debug transparent stuffs
+-- vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
+g.transparent_enabled = true
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,6 +4,15 @@ return {
     -- disable the keymap to grep files
     { "<leader>/", false },
     -- change a keymap
-    { "ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+    { "ff", "<cmd>Telescope find_files<cr>", desc = "find files" },
+    { "fg", "<cmd>Telescope live_grep<cr>", desc = "find word" },
+    --local builtin = require("telescope.builtin")
+    --vim.keymap.set("n", "ff", function()
+    --  return builtin.find_files({ hidden = true })
+    --end, {})
+    --vim.keymap.set("n", "fi", builtin.git_files, {})
+    --vim.keymap.set("n", "fg", builtin.live_grep, {})
+    --vim.keymap.set("n", "fb", builtin.buffers, {})
+    --vim.keymap.set("n", "fh", builtin.help_tags, {})
   },
 }
